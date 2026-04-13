b08aead9b82cd5b7c660734b00ccfed43c1642b9
Kryštof Absolon <krystof.absolon@gmail.com>
2026-04-13 14:37:10 +0200

    first commit

---
+++ src/main/java/io/github/mnohomarket_server/MnohomarketServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.mnohomarket_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnohomarketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnohomarketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mnohomarket_server/MnohomarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mnohomarket_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnohomarketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
