594e6d23e3a4e5f3d01fcf9dfb058554d17be255
Kryštof Absolon <krystof.absolon@gmail.com>
2026-04-20 15:49:03 +0200

    Stuff for single market page

--- src/main/java/io/github/mnohomarket_server/APIController.java
+++ src/main/java/io/github/mnohomarket_server/APIController.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.mnohomarket_server.Objects.Market;
+
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -18,6 +20,12 @@ public class APIController {
     public ArrayList<Market> getMethodName() {
         return MnohomarketServerApplication.markets;
     }
+
+    @GetMapping("/market")
+    public Market getMethodName(@RequestParam("m") int m) {
+        return MnohomarketServerApplication.markets.get(m - 1);
+    }
+    
     
     
 }
